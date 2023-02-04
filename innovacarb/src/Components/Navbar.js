8bf20fed6f2e312d35ef6717c73eeaa7cabc31b3
abrahamfigueroa <abraham.figueroaj@gmail.com>
2023-02-04 11:42:50 -0600

    added responsive functions to Navbar

--- innovacarb/src/Components/Navbar.js
+++ innovacarb/src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Logo from "../Components/Assets/logo-innovacarb.png";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
-import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import HomeIcon from "@mui/icons-material/Home"
 import InfoIcon from "@mui/icons-material/Info"
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
@@ -50,6 +50,24 @@ export const Navbar = () => {
       <div className='navbar-menu-container'>
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
       </div>
+
+    <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+    <Box sx={{ width: 250 }}
+    role="presentation"
+    onClick={() => setOpenMenu(false)}
+    onKeyDown={() => setOpenMenu(false)}>
+      <List>
+        {menuOptions.map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon> {item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+    </Drawer>
 
     </nav>
   )
