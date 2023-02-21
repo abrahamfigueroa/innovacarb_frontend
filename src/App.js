29451b98cef995e39a4316e6f7539b8f7e4a3f04
abrahamfigueroa <abraham.figueroaj@gmail.com>
2023-02-20 22:30:25 -0600

    fixed changes and conflicts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Landing } from './Components/Landing';
 import About from './Components/About';
 import Process from './Components/Process';
 import Questions from './Components/Questions';
-import CheckoutForm from './Components/CheckoutForm';
 import {stripePromise} from './Components/CheckoutForm'
 // import { useTheme } from '@mui/material/styles';
 // import { CssBaseline, makeStyles } from '@mui/material';
@@ -19,11 +18,8 @@ import {
 } from "react-router-dom";
 import Root from './Layout/Root';
 import Home from './pages/Home';
-// import { Home, Schedule } from '@mui/icons-material';
 import Schedules from './pages/Schedules';
-import {
-  Elements,
-} from "@stripe/react-stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -39,17 +35,6 @@ function App() {
   return (
     <div className='App'>
       <Home />
-      <About />
-      <Process />
-      <Elements stripe={stripePromise}>
-      <div className="container-fluid">
-        <div className="">
-          <div className="col-md-4 offset-md-4 h-100">
-            <CheckoutForm />
-          </div>
-        </div>
-      </div>
-    </Elements>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,15 +3,32 @@ import Landing from '../Components/Landing';
 import About from '../Components/About';
 import Process from '../Components/Process';
 import Questions from '../Components/Questions';
+import Root from '../Layout/Root';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import CheckoutForm from '../Components/CheckoutForm';
+import {Elements} from "@stripe/react-stripe-js";
+import {stripePromise} from '../Components/CheckoutForm'
 
 
 const Home = () => {
   return (
     <div>
+        <Navbar />
         <Landing />
         <About />
         <Process />
         <Questions />
+        <Elements stripe={stripePromise}>
+      <div className="container-fluid">
+        <div className="">
+          <div className="col-md-4 offset-md-4 h-100">
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
+        <Footer />
     </div>
   )
 }
