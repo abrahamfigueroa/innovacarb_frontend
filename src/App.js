1d5fc53b429d28618addba4c98381fa274faece2
Vryahn <b.ra@live.com.mx>
2023-02-23 21:01:46 -0600

    Adding not found page

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ const router = createBrowserRouter(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
       <Route path='schedules' element={<Schedules />} />
-      {/* ... etc. */}
+      <Route path='schedules' element={<Schedules />} />
+      {/* <Route path='login' element={<LogIn />} /> */}
+      <Route path='*' element={<p>Hijole joven, aquí no hay nada :C</p>} />
     </Route>
   )
 );
