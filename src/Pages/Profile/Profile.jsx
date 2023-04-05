6b6fe470b39dd7e80059b0a3b1003798962cb578
abrahamfigueroa <abraham.figueroaj@gmail.com>
2023-04-04 21:51:58 -0600

    added modal pop up to sign up coffeeshop

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -6,6 +6,11 @@ import SettingsProfile from '../../Components/SettingsProfile/SettingsProfile'
 import './Profile.css'
 import axios from 'axios'
 import jwt_decode from "jwt-decode";
+import BasicModal from '../../Components/BasicModal'
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 
 
@@ -16,21 +21,126 @@ const Profile = () => {
 
 const [coffeshopData, setCoffeshopData] = useState([])
 const [coffeshopId, setCoffeshopId] = useState("")
+const [openModal, setOpenModal] = useState(false);
+const valueToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2M2ZlYWZmMGY0NmRkZWQ4MDc5NTRmZTQiLCJyb2wiOiJjdXN0b21lciIsImlhdCI6MTY4MDY2MTg4OSwiZXhwIjoxNzEyMTk3ODg5fQ.WN9qaWifOUsuglmAVvSC3cr0QyBnpE60THq4guenous";
 
 
 useEffect(() => {
   axios
-  .get(`http://localhost:8000/coffeshop/63feaab44c9b6944055898e4`)
-  .then((res) =>{
-    console.log(res)
-    setCoffeshopData(res.data.payload)
-  })
-},[])
+    .get(`http://localhost:8000/users/63faaa39750596056d95ca6a`, {
+      headers: {
+        Authorization: `Bearer ${valueToken}`
+      }
+    })
+    .then((res) => {
+      console.log(res.data.payload.coffeshop.length);
+      setCoffeshopData(res.data.payload.coffeshop);
+    })
+}, []);
+
+const handleOpen = () => setOpenModal(true);
+const handleClose = () => setOpenModal(false);
 
 
   return (
-    !coffeshopData ? (<div>Cargando ... </div>) :  
+    coffeshopData.length === 0 ? (<> <h2>Aún no tienes cafeterías agregadas</h2><Button onClick={handleOpen}>Agregar cafetería</Button>
+          <BasicModal open={openModal} modalTitle={"Agrega una nueva cafetería"} close={handleClose}>
+      <Box component="form" noValidate sx={{ mt: 3 }}>
+      <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="nameCoffeeShop"
+                  label="Nombre de cafetería"
+                  name="nameCoffeeShop"
+                  autoComplete="Nombre de cafetería"
+                  margin="normal"
+                  // value={body.nameCoffeeShop}
+                  // onChange={inputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phoneNumber"
+                  type="number"
+                  label="Número telefónico"
+                  name="phoneNumber"
+                  autoComplete="Número telefónico"
+                  margin="normal"
+                  // value={body.phoneNumber}
+                  // onChange={inputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="address"
+                  label="Dirección de la cafetería"
+                  name="address"
+                  autoComplete="Dirección de la cafetería"
+                  margin="normal"
+                  // value={body.address}
+                  // onChange={inputChange}
+                />
+              </Grid>
+      </Box>
+        <Button>Registrar cafetería</Button>
+      </BasicModal>
+
+    </>) :  
     (<div className="MainProfileDiv">
+
+    <Button onClick={handleOpen}>Hola</Button>
+      <BasicModal open={openModal} modalTitle={"Agrega una nueva cafetería"} close={handleClose}>
+      <Box component="form" noValidate sx={{ mt: 3 }}>
+      <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="nameCoffeeShop"
+                  label="Nombre de cafetería"
+                  name="nameCoffeeShop"
+                  autoComplete="Nombre de cafetería"
+                  margin="normal"
+                  // value={body.nameCoffeeShop}
+                  // onChange={inputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phoneNumber"
+                  type="number"
+                  label="Número telefónico"
+                  name="phoneNumber"
+                  autoComplete="Número telefónico"
+                  margin="normal"
+                  // value={body.phoneNumber}
+                  // onChange={inputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="address"
+                  label="Dirección de la cafetería"
+                  name="address"
+                  autoComplete="Dirección de la cafetería"
+                  margin="normal"
+                  // value={body.address}
+                  // onChange={inputChange}
+                />
+              </Grid>
+      </Box>
+        <Button>Registrar cafetería</Button>
+      </BasicModal>
+
+  
 
       <div className="profile-container">
         <div className="top-section">
